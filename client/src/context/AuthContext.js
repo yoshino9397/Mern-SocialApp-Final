ab27c55f4497204e35822bf1188797a40f68f14b
矢山佳乃 <yayamayoshino@yayamayoshinonoMacBook-Air.local>
2022-04-02 02:16:28 -0700

    update authcontext

--- client/src/context/AuthContext.js
+++ client/src/context/AuthContext.js
@@ -1,4 +1,4 @@
-import { createContext, useReducer } from "react";
+import { createContext, useReducer, useEffect } from "react";
 import AuthReducer from "./AuthReducer";
 
 const INITIAL_STATE = {
@@ -13,6 +13,7 @@ const INITIAL_STATE = {
   //   followings: [],
   // },
   user: null,
+  // user:JSON.parse(localStorage.getItem("user")) || null,
   isFetching: false,
   error: false,
 };
@@ -21,6 +22,10 @@ export const AuthContext = createContext(INITIAL_STATE);
 
 export const AuthContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AuthReducer, INITIAL_STATE);
+  useEffect(()=>{
+    localStorage.setItem("user", JSON.stringify(state.user))
+  },[state.user])
+  
   return (
     <AuthContext.Provider
       value={{
